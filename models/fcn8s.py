5568f5bb48f7309915252b9bd35b4befe1e90e99
fighting! <37240099+lingtengqiu@users.noreply.github.com>
2021-04-27 23:44:57 +0800

    Update fcn8s.py

--- models/fcn8s.py
+++ models/fcn8s.py
@@ -3,7 +3,7 @@
 
 # **********************************************************
 # * Author        : lingteng qiu
-# * Email         : 1259738366@qq.com
+# * Email         : 
 # * Create time   : 2018-08-29 15:16
 # * Last modified : 2018-08-29 15:16
 # * Filename      : fcn8s.py
